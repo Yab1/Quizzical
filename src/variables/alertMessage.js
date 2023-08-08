7009e2bf13a8dd6f7fd9a46f2ca2ba21fe61c3bc
Yab1 <yabilisanu@gmail.com>
2023-08-08 03:57:30 +0300

    Refactor: Stored alert messages and types in variables for better organization and consistent management of alert messages throughout the application.

---
+++ src/variables/alertMessage.js
@@ -0,0 +1,13 @@
+export const ALERT_MESSAGES = {
+  SUCCESS_MESSAGE: "Congratulations, you scored full marks! Well done!",
+  WARNING_MESSAGE: "Almost there! Just a few more questions to go! 🚀",
+  ERROR_MESSAGE: "Oops! It looks like some of your answers are incorrect",
+  FAILED_TO_FETCH:
+    "Oops! Looks like you're offline. Please check your connection.",
+};
+
+export const ALERT_TYPE = {
+  SUCCESS_TYPE: "success",
+  WARNING_TYPE: "warning",
+  ERROR_TYPE: "error",
+};
